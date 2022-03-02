4fa09280e992658b723d5149d3257729abff178a
Dmitriy Starozhilov <dimstar-90@mail.ru>
2022-03-02 19:39:02 +0500

     Interface preparation

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSignal: UIView!
+    @IBOutlet var yellowSignal: UIView!
+    @IBOutlet var greenSignal: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSignal.layer.cornerRadius = 75
+        yellowSignal.layer.cornerRadius = 75
+        greenSignal.layer.cornerRadius = 75
+        
+        redSignal.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 15
     }
 
-
+    @IBAction func startButtonPressed() {
+        
+    }
+    
 }
 
